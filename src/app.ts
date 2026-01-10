7de87d00fecb7de8792ee09f6bd9aeb2cf1ea0cb
Israel de Souza <israellimas@hotmail.com>
2026-01-09 23:15:14 -0300

    feat: implement cors configs

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { errorMiddleware } from "./shared/error.middleware";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.send("Hello, World!");
 });
 
